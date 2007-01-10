3779269c19ddb434217640ffe378278cbdd8acd6
keyan <keyan@0fc55ab0-a1cc-4c11-8a76-5a057562e1de>
2007-01-10 06:02:20 +0000

    freemind mind map

--- trunk/doc/freemind/libRNN.mm
+++ trunk/doc/freemind/libRNN.mm
@@ -1,42 +1,178 @@
 <map version="0.8.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1168402688084" ID="Freemind_Link_1432908316" MODIFIED="1168402716676" TEXT="libRNN">
-<font NAME="FreeSans" SIZE="12"/>
-<node CREATED="1168402718719" ID="_" MODIFIED="1168402721920" POSITION="right" TEXT="Application">
-<node CREATED="1168402737157" ID="Freemind_Link_804836490" MODIFIED="1168402740051" TEXT="Evolution">
-<node CREATED="1168403152143" ID="Freemind_Link_1268480762" MODIFIED="1168403162390" TEXT="Very Fast Processing"/>
-<node CREATED="1168403162629" ID="Freemind_Link_154829316" MODIFIED="1168403177834" TEXT="Easy and fast paramter variation"/>
-<node CREATED="1168403178249" ID="Freemind_Link_1963919459" MODIFIED="1168403186048" TEXT="Easy and fast structure modifcation"/>
-<node CREATED="1168403189446" ID="Freemind_Link_50884682" MODIFIED="1168403194814" TEXT="Fast iteration over">
-<node CREATED="1168403196140" ID="Freemind_Link_1369095992" MODIFIED="1168403198512" TEXT="Neurons"/>
-<node CREATED="1168403198710" ID="Freemind_Link_1325109899" MODIFIED="1168403200548" TEXT="Synapses"/>
+<node COLOR="#000000" CREATED="1168402688084" ID="Freemind_Link_1432908316" MODIFIED="1168408832262" TEXT="libRNN">
+<font NAME="FreeSans" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<hook NAME="accessories/plugins/CreationModificationPlugin_new.properties"/>
+<node COLOR="#0033ff" CREATED="1168402718719" ID="_" MODIFIED="1168408831989" POSITION="right" TEXT="Application">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1168402737157" ID="Freemind_Link_804836490" MODIFIED="1168408832071" TEXT="Evolution">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1168403152143" ID="Freemind_Link_1268480762" MODIFIED="1168408832091" TEXT="Very Fast Processing">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1168403162629" ID="Freemind_Link_154829316" MODIFIED="1168408832092" TEXT="Easy and fast paramter variation">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1168403178249" ID="Freemind_Link_1963919459" MODIFIED="1168408832094" TEXT="Easy and fast structure modifcation">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1168403189446" ID="Freemind_Link_50884682" MODIFIED="1168408832096" TEXT="Fast iteration over">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1168403196140" ID="Freemind_Link_1369095992" MODIFIED="1168408832101" TEXT="Neurons"/>
+<node COLOR="#111111" CREATED="1168403198710" ID="Freemind_Link_1325109899" MODIFIED="1168408832101" TEXT="Synapses"/>
 </node>
 </node>
-<node CREATED="1168402740291" ID="Freemind_Link_605572392" MODIFIED="1168402742762" TEXT="Learning">
-<node CREATED="1168403230588" ID="Freemind_Link_1075693037" MODIFIED="1168403232731" TEXT="Global">
-<node CREATED="1168403246561" ID="Freemind_Link_1866759973" MODIFIED="1168403255095" TEXT="Backpropagation Like"/>
+<node COLOR="#00b439" CREATED="1168402740291" ID="Freemind_Link_605572392" MODIFIED="1168408832103" TEXT="Learning">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1168403230588" ID="Freemind_Link_1075693037" MODIFIED="1168408832108" TEXT="Global">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1168403246561" ID="Freemind_Link_1866759973" MODIFIED="1168408832111" TEXT="Backpropagation Like"/>
 </node>
-<node CREATED="1168403232956" ID="Freemind_Link_664578245" MODIFIED="1168403233724" TEXT="Local">
-<node CREATED="1168403235259" ID="Freemind_Link_135181650" MODIFIED="1168403237987" TEXT="Hebbian Like"/>
-<node CREATED="1168403238395" ID="Freemind_Link_1828372581" MODIFIED="1168403240278" TEXT="SRN Like"/>
-<node CREATED="1168403256622" ID="Freemind_Link_649926046" MODIFIED="1168403259239" TEXT="BCM Like"/>
-</node>
-</node>
-<node CREATED="1168402743124" ID="Freemind_Link_1836954258" MODIFIED="1168402763907" TEXT="Ambient Structure">
-<node CREATED="1168403267426" ID="Freemind_Link_1565009482" MODIFIED="1168403271736" TEXT="Feedforward"/>
-<node CREATED="1168403271993" ID="Freemind_Link_712660780" MODIFIED="1168403274231" TEXT="Hopfiled"/>
-<node CREATED="1168403274464" ID="Freemind_Link_1680058520" MODIFIED="1168403282085" TEXT="Tani">
-<node CREATED="1168403366647" ID="Freemind_Link_814149983" MODIFIED="1168403372427" TEXT="Modular"/>
-</node>
-<node CREATED="1168403282312" ID="Freemind_Link_523928496" MODIFIED="1168403283965" TEXT="Elamn"/>
-<node CREATED="1168403285789" ID="Freemind_Link_1361148106" MODIFIED="1168403293242" TEXT="Arbitraty"/>
-<node CREATED="1168403293463" ID="Freemind_Link_156164742" MODIFIED="1168403297001" TEXT="Highly connected"/>
-<node CREATED="1168403297214" ID="Freemind_Link_517272138" MODIFIED="1168403300032" TEXT="Loose coupled"/>
-<node CREATED="1168403373942" ID="Freemind_Link_865369314" MODIFIED="1168403377859" TEXT="Modular repetitive"/>
+<node COLOR="#990000" CREATED="1168403232956" ID="Freemind_Link_664578245" MODIFIED="1168408832112" TEXT="Local">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1168403235259" ID="Freemind_Link_135181650" MODIFIED="1168408832114" TEXT="Hebbian Like"/>
+<node COLOR="#111111" CREATED="1168403238395" ID="Freemind_Link_1828372581" MODIFIED="1168408832117" TEXT="SRN Like"/>
+<node COLOR="#111111" CREATED="1168403256622" ID="Freemind_Link_649926046" MODIFIED="1168408832122" STYLE="fork" TEXT="BCM Like"/>
 </node>
 </node>
-<node CREATED="1168402722470" ID="Freemind_Link_744157742" MODIFIED="1168402727878" POSITION="right" TEXT="Requirements"/>
-<node CREATED="1168402728359" ID="Freemind_Link_910502792" MODIFIED="1168402735836" POSITION="right" TEXT="Restrictions"/>
-<node CREATED="1168402885758" ID="Freemind_Link_1647387387" MODIFIED="1168402892372" POSITION="right" TEXT="Implementation"/>
+<node COLOR="#00b439" CREATED="1168402743124" ID="Freemind_Link_1836954258" MODIFIED="1168408832123" TEXT="Ambient Structure">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1168403267426" ID="Freemind_Link_1565009482" MODIFIED="1168408832142" TEXT="Feedforward">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1168403271993" ID="Freemind_Link_712660780" MODIFIED="1168408832143" TEXT="Hopfiled">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1168403274464" ID="Freemind_Link_1680058520" MODIFIED="1168408832145" TEXT="Tani">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1168403366647" ID="Freemind_Link_814149983" MODIFIED="1168408832146" TEXT="Modular"/>
+</node>
+<node COLOR="#990000" CREATED="1168403282312" ID="Freemind_Link_523928496" MODIFIED="1168408832147" TEXT="Elamn">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1168403285789" ID="Freemind_Link_1361148106" MODIFIED="1168408832147" TEXT="Arbitraty">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1168403293463" ID="Freemind_Link_156164742" MODIFIED="1168408832148" TEXT="Highly connected">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1168403297214" ID="Freemind_Link_517272138" MODIFIED="1168408832149" TEXT="Loose coupled">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1168403373942" ID="Freemind_Link_865369314" MODIFIED="1168408832149" TEXT="Modular repetitive">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1168406379703" ID="Freemind_Link_693750414" MODIFIED="1168408832154" TEXT="Few parameter to specifiy net">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1168408132456" ID="Freemind_Link_1442820894" MODIFIED="1168408832154" TEXT="Logging">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1168408147790" ID="Freemind_Link_66836306" MODIFIED="1168408832156" TEXT="For Analysis">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1168408157417" ID="Freemind_Link_1377616442" MODIFIED="1168408832157" TEXT="For debugging">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1168408616415" ID="Freemind_Link_529340152" MODIFIED="1168408832157" TEXT="XML / XSD">
+<edge WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1168408621003" ID="Freemind_Link_969605087" MODIFIED="1168408832160" TEXT="Generic">
+<edge WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1168408626072" ID="Freemind_Link_1190496784" MODIFIED="1168408832172" TEXT="Model Change no XSD Change">
+<edge WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1168402722470" ID="Freemind_Link_744157742" MODIFIED="1168408832172" POSITION="right" TEXT="Requirements">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1168404111542" ID="Freemind_Link_422548497" MODIFIED="1168408832193" TEXT="extandable">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1168404120801" ID="Freemind_Link_154224387" MODIFIED="1168408832202" TEXT="Neuron Model">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1168406417922" ID="Freemind_Link_331738661" MODIFIED="1168408832208" TEXT="Transferfunction"/>
+<node COLOR="#111111" CREATED="1168406422954" ID="Freemind_Link_424864060" MODIFIED="1168408832208" TEXT="Properties"/>
+</node>
+<node COLOR="#990000" CREATED="1168404123001" ID="Freemind_Link_478587558" MODIFIED="1168408832211" TEXT="Synapse Model">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1168406428119" ID="Freemind_Link_682474417" MODIFIED="1168408832215" TEXT="static"/>
+<node COLOR="#111111" CREATED="1168406429799" ID="Freemind_Link_558666840" MODIFIED="1168408832216" TEXT="dynamic">
+<node COLOR="#111111" CREATED="1168408181302" ID="Freemind_Link_207121644" MODIFIED="1168408832217" TEXT="function of pre- and post-synaptic neuron"/>
+<node COLOR="#111111" CREATED="1168408190442" ID="Freemind_Link_1219763366" MODIFIED="1168408832219" TEXT="function of synapse property"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1168404126634" ID="Freemind_Link_1788391583" MODIFIED="1168408832220" TEXT="Net Model">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1168406433561" ID="Freemind_Link_1561883190" MODIFIED="1168408832222" TEXT="global transferfunction"/>
+<node COLOR="#111111" CREATED="1168406439986" ID="Freemind_Link_125132886" MODIFIED="1168408832223" TEXT="processing order of neurons/synapses"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1168404137178" ID="Freemind_Link_1657554819" MODIFIED="1168408832224" TEXT="efficient">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1168404147988" ID="Freemind_Link_1526340459" MODIFIED="1168408832228" TEXT="Memory">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1168404152242" ID="Freemind_Link_1073172047" MODIFIED="1168408832229" TEXT="Computational cost">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1168402885758" ID="Freemind_Link_1647387387" MODIFIED="1168408832229" POSITION="right" TEXT="Implementation">
+<edge WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1168404086063" ID="Freemind_Link_86016646" MODIFIED="1168408832237" TEXT="Matrix">
+<edge WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1168404087788" ID="Freemind_Link_21365706" MODIFIED="1168408832238" TEXT="Graph">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1168404090183" ID="Freemind_Link_1187635854" MODIFIED="1168408832242" TEXT="Neuron oriented">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1168404100808" ID="Freemind_Link_1249015962" MODIFIED="1168408832243" TEXT="Net oriented">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1168406116761" ID="Freemind_Link_1809905186" MODIFIED="1168408832244" TEXT="Synapse oriented">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1168406124559" ID="Freemind_Link_868169197" MODIFIED="1168408832245" TEXT="Vector">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1168406135066" ID="Freemind_Link_601241571" MODIFIED="1168408832248" TEXT="List">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1168408000728" ID="Freemind_Link_1948033882" MODIFIED="1168408832249" TEXT="Threads">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1168408008785" ID="Freemind_Link_68515562" MODIFIED="1168408832249" TEXT="Thread-safe">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1168408506859" ID="Freemind_Link_1508758635" MODIFIED="1168408832250" POSITION="right" TEXT="Tests">
+<edge WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1168408528621" ID="Freemind_Link_1582479705" MODIFIED="1168408832256" TEXT="Runtime">
+<edge WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1168408533363" ID="Freemind_Link_232256432" MODIFIED="1168408832256" TEXT="Memory Consumption">
+<edge WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1168408556333" ID="Freemind_Link_1094615111" MODIFIED="1168408832257" TEXT="Test Driven Developement">
+<edge WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1168408569279" ID="Freemind_Link_210091301" MODIFIED="1168408832262" TEXT="TestCases">
+<edge WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
 </node>
 </map>
