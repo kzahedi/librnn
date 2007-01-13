ea80c210dc0d4fbd2e2335af4e31c540ba597da9
keyan <keyan@0fc55ab0-a1cc-4c11-8a76-5a057562e1de>
2007-01-13 19:45:09 +0000

    source files for librnn implementation so far for testing   - log4cpp   - testSuiote   - benchmarking

---
+++ trunk/src/RecurrentNeuralNetwork.cpp
@@ -0,0 +1,41 @@
+#include <librnn/librnn.h>
+
+#include <utility>                   // for std::pair
+#include <algorithm>                 // for std::for_each
+#include <boost/graph/dijkstra_shortest_paths.hpp>
+
+
+
+using namespace std;
+
+RecurrentNeuralNetwork::RecurrentNeuralNetwork()
+{
+#ifdef USE_LOG4CPP_OUTPUT
+  libRnnLogger.debug("constructor called");
+#endif
+  printf("constructor called\n");
+  _numberOfSynapses = -1;
+  _numberOfNeurons  = -1;
+}
+
+RecurrentNeuralNetwork::~RecurrentNeuralNetwork()
+{
+}
+
+void RecurrentNeuralNetwork::init(int numberOfNeurons, int numberOfSynapses)
+{
+#ifdef USE_LOG4CPP_OUTPUT
+  libRnnLogger.debug("number of neurons : %d", numberOfNeurons);
+  libRnnLogger.debug("number of synapses: %d", numberOfSynapses);
+#endif
+}
+
+int RecurrentNeuralNetwork::getNumberOfNeurons()
+{
+  return _numberOfNeurons;
+}
+
+int RecurrentNeuralNetwork::getNumberOfSynapses()
+{
+  return _numberOfSynapses;
+}
