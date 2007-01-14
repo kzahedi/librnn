17a7e4a713148af780f3a66ccb93c3222dd268e3
keyan <keyan@0fc55ab0-a1cc-4c11-8a76-5a057562e1de>
2007-01-14 04:16:29 +0000

    basic Neuron implementation for testing

---
+++ include/librnn/Neuron.h
@@ -0,0 +1,17 @@
+#ifndef __NEURON_H__
+#define __NEURON_H__
+
+#include <librnn/librnn.h>
+
+namespace librnn 
+{
+  class Neuron
+  {
+
+    private:
+      dReal activation;
+      dReal output;
+  };
+}
+
+#endif // __NEURON_H__
---
+++ src/Neuron.cpp
@@ -0,0 +1,1 @@
+#include <librnn/librnn.h>
