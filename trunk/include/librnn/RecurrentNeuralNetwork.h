5a3d3f4c4e673e3a5ce35d79dbb292058a06532d
keyan <keyan@0fc55ab0-a1cc-4c11-8a76-5a057562e1de>
2007-04-08 21:59:55 +0000

    switching to cmake

--- trunk/include/librnn/RecurrentNeuralNetwork.h
+++ trunk/include/librnn/RecurrentNeuralNetwork.h
@@ -10,6 +10,30 @@ using namespace std;
 
 namespace librnn
 {
+  class RecurrentNeuralNetworkException : public std::exception
+  {
+    public:
+      explicit RecurrentNeuralNetworkException(const std::string& what)
+        :
+          m_what(what)
+    {}
+
+      virtual ~RecurrentNeuralNetworkException() throw() {}
+
+      virtual const char * what() const throw()
+      {
+        return m_what.c_str();
+      }
+
+      virtual void message() const throw()
+      {
+        cerr << "RecurrentNeuralNetworkException: " << m_what << endl;
+      }
+
+    private:
+      std::string m_what;
+  };
+
 
   class RecurrentNeuralNetwork 
   {
@@ -19,18 +43,26 @@ namespace librnn
 
       int getNumberOfNeurons();
 
-      bool addNeuron(Neuron *neuron);
+      void addNeuron(Neuron *neuron);
+      void addSynapse(Synapse *synapse);
+      int  getSynapsesCount();
+      int  countSynapses();
 
     private:
+      int _numberOfSynapses;
 
 #ifdef IMPL_ADJ_LIST
       slist<Neuron*> _neurons;
+      slist<Neuron*>::iterator _neuronIterator;
       int _numberOfNeurons;
 #endif
 
 #ifdef IMPL_ADJ_VECTOR
       vector<Neuron*> _neurons;
+      vector<Neuron*>::iterator _neuronIterator;
 #endif
+
+
 
   };
 }
